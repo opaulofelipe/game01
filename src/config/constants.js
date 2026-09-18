6039ee7f74df7ef1df76edc066f3b6e884ec9598
Paulo Felipe <80161893+opaulofelipe@users.noreply.github.com>
2026-09-18 15:58:07 -0300

    Add A Casa project files

---
+++ src/config/constants.js
@@ -0,0 +1,26 @@
+export const W = 960;
+export const H = 540;
+
+export const C = {
+  ink: 0x171513,
+  ink2: 0x27221e,
+  paper: 0xe8dfd1,
+  paper2: 0xcfc4b5,
+  warm: 0xb99c76,
+  warmDark: 0x765f46,
+  floor: 0x9f8d75,
+  floor2: 0x8d7b66,
+  wall: 0x4c443b,
+  wallDark: 0x2e2925,
+  wood: 0x574638,
+  woodLight: 0x7b6550,
+  sofa: 0x68716a,
+  green: 0x697a67,
+  red: 0x8b5f55,
+  blue: 0x607485,
+  memory: 0xc2a878,
+  memoryDark: 0x594a36,
+};
+
+export const PLAYER_SPEED = 150;
+export const SAVE_KEY = "a-casa-save-v1";
